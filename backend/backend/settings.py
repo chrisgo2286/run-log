b0dd1a8ddaf7951b3d20eb3548e09cbf4b83aac0
Go <christian.go@zoetis.com>
2023-01-29 08:23:53 -0700

    Backend: created Profile model

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'runlog',
+    'profiles',
     'accounts',
     'corsheaders',
     'rest_framework',
--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+GENDER_CHOICES = (
+    ('Male', 'Male'),
+    ('Female', 'Female')
+)
+
+class Profile(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    age = models.IntegerField(blank=True, null=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, default='Male')
+    email = models.EmailField(max_length=100, blank=True, null=True)
+    preference = models.TextField(blank=True, null=True) 
+    history = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    @property
+    def username(self):
+        return self.User.username
+
+    def __str__(self):
+        return f'{self.username} - Profile'
