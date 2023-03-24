19e092644f356494674940b22735ae06a01ab2f0
Go <christian.go@zoetis.com>
2023-03-23 23:08:25 -0600

    Backend: create RunTypes to pull data for run type composition

---
+++ backend/stats/misc_scripts/run_types.py
@@ -0,0 +1,19 @@
+from stats.misc_scripts.misc_functions import calc_mileage
+
+class RunTypes:
+    """Class to provide data for run type composition"""
+    RUN_TYPES = ['Easy Run', 'Long Run', 'Intervals', 'Tempo']
+    def __init__(self, runs):
+        self.runs = runs
+        self.data = []
+
+    def pull_data(self):
+        """Populates data list with run type and corresponding mileage total"""
+        for run_type in self.RUN_TYPES:
+            runs = self.filter_runs_by_type(run_type)
+            mileage = calc_mileage(runs)
+            self.data.append({'run_type': run_type, 'distance': mileage})
+
+    def filter_runs_by_type(self, run_type):
+        """Returns queryset of runs for given run_type"""
+        return self.runs.filter(run_type=run_type)
--- backend/stats/misc_scripts/stats.py
+++ backend/stats/misc_scripts/stats.py
@@ -2,6 +2,7 @@ from runlog.models import Run
 from stats.misc_scripts.summary_stats import SummaryStats
 from stats.misc_scripts.monthly_mileage import MonthlyMileage
 from stats.misc_scripts.weekly_mileage import WeeklyMileage
+from stats.misc_scripts.run_types import RunTypes
 
 class Stats:
     """Class to compile summary stats and charts into dict"""
@@ -16,7 +17,7 @@ class Stats:
         self.compile_summary()
         self.compile_monthly_mileage_chart()
         self.compile_weekly_mileage_chart()
-        self.compile_chartC()
+        self.compile_run_type_chart()
 
     def compile_summary(self):
         """Compile summary stats"""
@@ -36,8 +37,10 @@ class Stats:
         weekly_mileage.pull_data()
         self.data['weekly_chart'] = weekly_mileage.data
 
-    def compile_chartC(self):
+    def compile_run_type_chart(self):
         """Compiles Rechart data"""
-        pass
+        run_types = RunTypes(self.runs)
+        run_types.pull_data()
+        self.data['run_types'] = run_types.data
 
     
