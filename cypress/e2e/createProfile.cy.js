bfb09b6b7b4591dc39efc6110a8c218d63af4b1d
Go <christian.go@zoetis.com>
2023-04-14 23:34:15 -0600

    Cypress: created selectors.json fixture

--- cypress/e2e/createProfile.cy.js
+++ cypress/e2e/createProfile.cy.js
@@ -1,18 +1,6 @@
-import { validationErrors, age, gender, email } from '../fixtures/data.json'
-
-const ageField = 'input[data-cy="profile-age"]'
-const genderField = 'select[data-cy="profile-gender"]'
-const emailField = 'input[data-cy="profile-email"]'
-const preferenceField = 'textarea[data-cy="profile-preference"]'
-const historyField = 'textarea[data-cy="profile-history"]'
-const descriptionField = 'textarea[data-cy="profile-description"]'
-const togglePrivacy = 'div[data-cy="toggle-button"]'
-const privacy = 'div[data-cy="privacy-choice"]'
-const submitButton = 'div[data-cy="create-profile-button"]'
-
-const ageError = 'Please enter a valid age!'
-const genderError = 'Please enter your gender!'
-const emailError = 'Please enter a valid email!'
+import { testUser } from '../fixtures/testUser.json'
+import { selectors } from '../fixtures/selectors.json'
+import { errors } from '../fixtures/errors.json'
 
 describe('CreateProfile page', () => {
     beforeEach(() => {
@@ -21,39 +9,39 @@ describe('CreateProfile page', () => {
     })
 
     it('Correct fields are rendered', () => {
-        cy.get(ageField).should('exist')
-        cy.get(genderField).should('exist')
-        cy.get(emailField).should('exist')
-        cy.get(preferenceField).should('exist')
-        cy.get(historyField).should('exist')
-        cy.get(descriptionField).should('exist')
-        cy.get(togglePrivacy).should('exist')
-        cy.get(submitButton).should('exist')
+        cy.get(selectors.ageField).should('exist')
+        cy.get(selectors.genderField).should('exist')
+        cy.get(selectors.emailField).should('exist')
+        cy.get(selectors.preferenceField).should('exist')
+        cy.get(selectors.historyField).should('exist')
+        cy.get(selectors.descriptionField).should('exist')
+        cy.get(selectors.togglePrivacy).should('exist')
+        cy.get(selectors.submitButton).should('exist')
     })
 
     it('Clicking privacy toggle switches to Public', () => {
-        cy.get(togglePrivacy).click()
-        cy.contains(privacy, 'Public')
+        cy.get(selectors.togglePrivacy).click()
+        cy.contains(selectors.privacy, 'Public')
     })
 
     it('Validation functions correctly', () => {
-        cy.get(ageField).type('38X')
-        cy.get(emailField).type('chrisgo2286yahoo.com')
-        cy.get(submitButton).click()
-        cy.get(validationErrors).children().should(($error) => {
-            expect($error.eq(0)).to.contain(ageError)
-            expect($error.eq(1)).to.contain(genderError)
-            expect($error.eq(2)).to.contain(emailError)
+        cy.get(selectors.ageField).type('38X')
+        cy.get(selectors.emailField).type('chrisgo2286yahoo.com')
+        cy.get(selectors.submitButton).click()
+        cy.get(selectors.validationErrors).children().should(($error) => {
+            expect($error.eq(0)).to.contain(errors.ageError)
+            expect($error.eq(1)).to.contain(errors.genderError)
+            expect($error.eq(2)).to.contain(errors.emailError)
         })
     })
 
     it('Submitting data navigates to UserProfile; shows correct data', () => {
         cy.interceptPostProfile()
         cy.interceptGetUserProfile()
-        cy.get(ageField).type(age)
-        cy.get(genderField).select(gender)
-        cy.get(emailField).type(email)
-        cy.get(submitButton).click()
+        cy.get(selectors.ageField).type(testUser.age)
+        cy.get(selectors.genderField).select(testUser.gender)
+        cy.get(selectors.emailField).type(testUser.email)
+        cy.get(selectors.submitButton).click()
         cy.url().should('include', 'user_profile')
     })
 })
